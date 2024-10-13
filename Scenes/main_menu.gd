cd2ce627c72617ba7ecb95e57fde3ac151785618
M1kikralj <malosutra424@gmail.com>
2024-10-13 22:30:02 +0200

    YIPPIE
    Main menu completed

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -3,13 +3,13 @@ extends Control
 
 
 
-func _on_play_pressed() -> void:
+func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
 
-func _on_options_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/options_menu.tscn")
+func _on_button_2_pressed() -> void:
+	pass # Replace with function body.
 
 
-func _on_quit_pressed() -> void:
+func _on_button_3_pressed() -> void:
 	get_tree().quit()
